9bf5dd4c724b66b94366638845c4d8eea48b18c7
Adam Wespiser <adamwespiser@gmail.com>
2015-06-05 09:08:44 -0400

    move things around

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 sudo apt-get install gnuradio
 sudo apt-get install rtl-sdr
 sudo apt-get install gnome-radio
+# gps 
+sudo apt-get install gpsd python-gps gpsdrive viking gpsd-clients libgps-dev libgtk2.0-dev glutg3-dev libcegui-mk2-dev libxmu-dev libsdl-dev libpcre3-dev libspeechd-dev libtiff-dev libdevil-dev build-essential subversion automak
+sudo apt-get install viking foxtrotgps python-gps gqrx-sdr libitpp-dev gpxviewer
+
