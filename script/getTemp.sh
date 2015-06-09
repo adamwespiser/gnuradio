849685a1c487eb0c54f132534b74dae714570187
Adam Wespiser <adamwespiser@gmail.com>
2015-06-08 23:28:08 -0400

    temp script

---
+++ script/getTemp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
+cpuTemp1=$(($cpuTemp0/1000))
+cpuTemp2=$(($cpuTemp0/100))
+cpuTempM=$(($cpuTemp2 % $cpuTemp1))
+stamp=$(date)
+unixEpoch=$(date +%s)
+
+
+echo CPU temp"="$cpuTemp1"."$cpuTempM"'C"
